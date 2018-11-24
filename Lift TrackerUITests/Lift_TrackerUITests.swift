fef7d6cca07ac737f2ecedd4fa729545bf9c4d0e
Chase Burnham <cburnhamiv@gmail.com>
2018-11-24 16:21:05 -0700

    updated name added snap shot tests

--- Lift TrackerUITests/Lift_TrackerUITests.swift
+++ Lift TrackerUITests/Lift_TrackerUITests.swift
@@ -26,10 +26,29 @@ class Lift_TrackerUITests: XCTestCase {
         super.tearDown()
     }
     
+    func login() {
+        let emailButton = app/*@START_MENU_TOKEN@*/.buttons["EmailButtonAccessibilityID"]/*[[".buttons[\"Sign in with email\"]",".buttons[\"EmailButtonAccessibilityID\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        if (emailButton.exists) {
+            emailButton.tap()
+            let tablesQuery = app.tables
+            let emailField = tablesQuery/*@START_MENU_TOKEN@*/.textFields["Enter your email"]/*[[".cells[\"EmailCellAccessibilityID\"].textFields[\"Enter your email\"]",".textFields[\"Enter your email\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+            emailField.tap()
+            emailField.typeText("test@test.com")
+            
+            let passwordField = tablesQuery/*@START_MENU_TOKEN@*/.secureTextFields["Enter your password"]/*[[".cells.secureTextFields[\"Enter your password\"]",".secureTextFields[\"Enter your password\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+            passwordField.tap()
+            passwordField.typeText("test1234")
+            app.navigationBars["Sign in"].buttons["Sign in"].tap()
+        }
+    }
+    
     func testExample() {
-        snapshot("HomeScreen")
-
+        snapshot("Home")
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["Bench"]/*[[".cells.staticTexts[\"Bench\"]",".staticTexts[\"Bench\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         
+        snapshot("AddSet")
+        app.buttons["Past Lifts"].tap()
+        snapshot("PastSets")
     }
     
 }
