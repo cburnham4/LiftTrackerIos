dd7c304a33142691eb01a9efe685c5856a47da82
Chase Burnham <cburnhamiv@gmail.com>
2018-11-24 14:54:49 -0700

    added fastlane snapshot

--- Lift TrackerUITests/Lift_TrackerUITests.swift
+++ Lift TrackerUITests/Lift_TrackerUITests.swift
@@ -9,18 +9,16 @@
 import XCTest
 
 class Lift_TrackerUITests: XCTestCase {
+    
+    let app = XCUIApplication()
         
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
+        setupSnapshot(app)
+        app.launch()
     }
     
     override func tearDown() {
@@ -29,7 +27,7 @@ class Lift_TrackerUITests: XCTestCase {
     }
     
     func testExample() {
-        
+        snapshot("HomeScreen")
 
         
     }
